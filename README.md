62ff52e584358ac3a40b083e637da019e1a8a6c2
Robin Hawkes <rob@rawkes.com>
2016-02-10 14:12:15 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Polygon City CLI Multi-Directory
+
+## Requirements
+
+1. npm and Node v5 (https://nodejs.org/dist/v5.2.0/node-v5.2.0.pkg)
+2. Redis (http://redis.io/download#installation)
+3. A Mapzen Elevation Service key (https://mapzen.com/developers)
+
+## Installation
+
+```bash
+$ npm install polygon-city-cli-multi@latest -g
+```
+
+## Usage
+
+Processing a new CityGML file:
+
+```bash
+$ polygon-city-multi -e <epsg_code> -m <mapzen_elevation_key> -o /path/to/obj/output/directory /path/to/cityGml/input/directory
+```
+
+Resuming previous unfinished jobs:
+
+```bash
+$ polygon-city-multi resume
+```
+
+## Step-by-step example
+
+1. Run `npm install polygon-city-cli-multi -g`
+2. [Start a local Redis server](http://redis.io/topics/quickstart#starting-redis) (using port 6379)
+3. [Download some CityGML data from Linz, Austria](http://geo.data.linz.gv.at/katalog/geodata/3d_geo_daten_lod2/)
+4. Take note of [the EPSG code used for the Linz dataset](http://geo.data.linz.gv.at/katalog/geodata/3d_geo_daten_lod2/Beschreibung.txt) (EPSG:31255)
+5. Run the command line tool:
+
+```bash
+$ polygon-city-multi -e 31255 -m "your_mapzen_key" -o /path/to/obj/output/directory /path/to/cityGml/input/directory
+```
---
+++ index.js
@@ -0,0 +1,170 @@
+// TODO: Allow CLI options to be defined and passed through
+
+var Promise = require('bluebird');
+var async = require('async');
+var program = require('commander');
+var path = require('path');
+var fs = Promise.promisifyAll(require('fs'));
+var chalk = require('chalk');
+var childProcess = require('child_process');
+var shell = require('shelljs');
+var turf = require('turf');
+
+// Check for CLI binary
+if (!shell.which("polygon-city")) {
+  console.error(chalk.bgRed.gray('Error: Polygon City CLI not found'));
+  process.exit();
+}
+
+var processFiles = function(inputDirectory, options) {
+  console.log('EPSG: %j', options.epsg);
+  console.log('Mapzen key: %j', options.mapzen);
+
+  if (options.prefix) {
+    console.log('Prefix: %j', options.prefix);
+  }
+
+  console.log('Output directory: %j', program.output);
+  console.log('Input directory: %j', inputDirectory);
+
+  // Check input file path is defined
+  if (!inputDirectory) {
+    console.error(chalk.red('Exiting: Input file path not specified'));
+    process.exit(1);
+  }
+
+  // Check output file path is defined
+  if (!options.output) {
+    console.error(chalk.red('Exiting: Output file path not specified'));
+    process.exit(1);
+  }
+
+  // Check EPSG code
+  if (!options.epsg) {
+    console.error(chalk.red('Exiting: EPSG code not specified'));
+    process.exit(1);
+  }
+
+  // Check Mapzen key
+  if (!options.mapzen) {
+    console.error(chalk.red('Exiting: Mapzen Elevation key not specified'));
+    process.exit(1);
+  }
+
+  fs.readdirAsync(inputDirectory).then((files) => {
+    var ext, args, child;
+
+    // Spawn a new CLI instance for each file, but only after the previous
+    // one has completed
+    files.forEach((file) => {
+      ext = path.extname(file);
+      bareName = file.split(ext)[0];
+
+      if (ext !== '.gml') {
+        return;
+      }
+
+      args = [];
+
+      args.push('-e');
+      args.push(options.epsg);
+      args.push('-m');
+      args.push(options.mapzen);
+
+      if (options.prefix) {
+        args.push('-p');
+        args.push(options.prefix);
+      }
+
+      args.push('-o');
+
+      var output = (options.prefix) ? options.prefix + bareName : bareName;
+
+      args.push(path.join(options.output, output));
+
+      args.push(path.join(inputDirectory, file));
+
+      console.log(chalk.bgGreen.gray('Spawning polygon-city process...'));
+      console.log(args);
+
+      child = childProcess.spawnSync('polygon-city', args, {
+        killSignal: 'SIGINT'
+      });
+    });
+
+    var outputDirs = fs.readdirSync(options.output).filter(function(file) {
+      return fs.statSync(path.join(options.output, file)).isDirectory();
+    });
+
+
+    // Gather GeoJSON files and create a combined index that has a polygon for
+    // each directory showing the area it covers
+
+    var geojsonIndexPaths = outputDirs.map(dir => {
+      var geojsonPath = path.join(options.output, dir, 'index.geojson');
+
+      try {
+        fs.statSync(geojsonPath);
+        return geojsonPath;
+      } catch(err) {
+        return;
+      }
+    });
+
+    // Remove empties
+    geojsonIndexPaths = geojsonIndexPaths.filter(function(n) { return n != undefined });
+
+    // console.log(geojsonIndexPaths);
+
+    // Get GeoJSON indexes
+    async.map(geojsonIndexPaths, fs.readFile, (err, results) => {
+      var envelopes = [];
+
+      if (err) {
+        throw err;
+      }
+
+      var relativePath;
+      results.forEach((result, index) => {
+        relativePath = path.relative(options.output, geojsonIndexPaths[index]);
+        envelope = turf.envelope(JSON.parse(result.toString()));
+
+        envelope.properties.id = relativePath.split('/index.geojson')[0];
+        envelope.properties.path = path.relative(options.output, geojsonIndexPaths[index]);
+
+        envelopes.push(envelope);
+      });
+
+      var fc = turf.featurecollection(envelopes);
+
+      fs.writeFileAsync(path.join(options.output, 'index.geojson'), JSON.stringify(fc)).then(result => {
+        console.log(chalk.bgBlue.gray('All files processed. Exiting...'));
+      });
+    });
+  }).catch((err) => {
+    console.error(chalk.bgRed.gray(err));
+    process.exit();
+  });
+};
+
+var resumeJobs = function() {
+  var child = childProcess.spawnSync('polygon-city', ['resume'], {
+    killSignal: 'SIGINT'
+  });
+};
+
+program
+  .version('0.0.1')
+  .usage('[options] <input directory>')
+  .option('-e, --epsg [code]', 'EPSG code for input data')
+  .option('-m, --mapzen [key]', 'Mapzen Elevation API key')
+  .option('-p, --prefix [prefix]', 'Prefix for building IDs')
+  .option('-o, --output [directory]', 'Output directory')
+  .action(processFiles);
+
+program
+  .command('resume')
+  .description('Resume processing of existing jobs')
+  .action(resumeJobs);
+
+program.parse(process.argv);
